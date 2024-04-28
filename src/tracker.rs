c2a8001bbf2b503c25ad99e4dbd222e46ba4dd50
William Findlay <william@williamfindlay.com>
2024-04-27 21:16:13 -0400

    fix accuracy bug and improve performance

--- src/tracker.rs
+++ src/tracker.rs
@@ -10,6 +10,7 @@ const WINDOW_SIZE: usize = 60;
 #[derive(Debug)]
 struct TrackerInner {
     actions: [u64; WINDOW_SIZE],
+    rolling_count: u64,
     tick: usize,
 }
 
@@ -17,6 +18,7 @@ impl Default for TrackerInner {
     fn default() -> Self {
         Self {
             actions: [0u64; WINDOW_SIZE],
+            rolling_count: 0,
             tick: Default::default(),
         }
     }
@@ -39,23 +41,48 @@ impl Tracker {
 
     pub async fn increment_tick(&self) {
         let mut inner = self.inner.lock().await;
+
+        let to_add = inner.actions[inner.tick % WINDOW_SIZE];
+        inner.rolling_count += to_add;
+
+        let mut to_sub = None;
+        if inner.tick >= WINDOW_SIZE {
+            to_sub = Some(inner.actions[(inner.tick - WINDOW_SIZE + 1) % WINDOW_SIZE]);
+            inner.rolling_count -= to_sub.unwrap();
+        }
+
+        tracing::debug!(
+            inner.tick,
+            to_add,
+            to_sub = to_sub.unwrap_or(0),
+            inner.rolling_count,
+            "increment_tick"
+        );
+
         inner.tick += 1;
         let tick = inner.tick;
         inner.actions[tick % WINDOW_SIZE] = 0;
-        tracing::debug!(new = tick, "increment_tick");
     }
 
     pub async fn apm(&self) -> u64 {
         let inner = self.inner.lock().await;
-        let mut acc = 0;
-        for v in inner.actions {
-            acc += v;
+        if inner.tick == 0 {
+            return 0;
         }
-        if inner.tick > 0 && inner.tick < WINDOW_SIZE {
-            let m = WINDOW_SIZE / inner.tick;
-            acc = acc * m as u64;
+        if inner.tick < 60 {
+            let m = WINDOW_SIZE as f64 / inner.tick as f64;
+            return (m * inner.rolling_count as f64).round() as u64;
         }
-        acc
+        inner.rolling_count
+        // let mut acc = 0;
+        // for v in inner.actions {
+        //     acc += v;
+        // }
+        // if inner.tick > 0 && inner.tick < WINDOW_SIZE {
+        //     let m = WINDOW_SIZE / inner.tick;
+        //     acc = acc * m as u64;
+        // }
+        // acc
     }
 
     pub async fn track(self: Arc<Self>, rchan: Arc<Mutex<UnboundedReceiver<()>>>) -> Result<()> {
